8a23a190ac505d9eacbe569f9ba0aef146cd364b
stas <chousetheone@gmail.com>
2024-08-21 19:11:58 +0300

    Добавлены миграции

--- bot/config.py
+++ bot/config.py
@@ -3,11 +3,15 @@ from telegram import Bot, Update
 from telegram.ext import Application, ContextTypes, CommandHandler
 
 
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = '7421914146:AAHKaKWoyhIkQPakzIH_7MNp8hDiLFfKbUg'
 bot = Bot(TOKEN)
-application = Application.builder().token(TOKEN).build()
+application = Application.builder().token(TOKEN).connect_timeout(60).build()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Welcome to TicTacToe game Bot!")
 
 application.add_handler(CommandHandler("start", start))
+
+
+if __name__ == '__main__':
+    application.run_polling()
